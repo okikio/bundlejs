cb9f687702d3e2e20a4ba8b139f3befe427d9b57
Okiki <femikiki@gmail.com>
2021-05-01 04:26:37 -0400

    chore: remove theme.ts import

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,9 +1,5 @@
 import * as Default from "./modules/default";
 import * as Playground from "./modules/playground";
-import { runTheme } from "./modules/theme";
-
-// Theme change
-runTheme();
 
 // The default navbar, etc... that is needed
 Default.build();
