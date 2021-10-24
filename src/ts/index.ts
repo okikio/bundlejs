11815e28dfe9e4ed51edad205166a80ed9e68807
Okiki <okikio.dev@gmail.com>
2021-10-24 06:59:43 +0000

    perf: try boosting perf on mobile devices

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -340,3 +340,5 @@ import { hit } from "countapi-js";
         );
     }
 })();
+
+export { };
--- src/ts/scripts/load-webanimation-polyfill.ts
+++ src/ts/scripts/load-webanimation-polyfill.ts
@@ -1,7 +1,7 @@
 if (!window.KeyframeEffect) {
     let script = document.createElement("script");
     // https://cdn.jsdelivr.net/npm/web-animations-js/web-animations-next.min.js
-    script.src = "./js/webanimation.min.js";
+    script.src = "/js/webanimation.min.js";
     document.head.appendChild(script);
 }
 
