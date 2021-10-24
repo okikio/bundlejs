3377f7ff442b205c1624a0a2eb5d33cf6829b9d4
Okiki <okikio.dev@gmail.com>
2021-10-24 06:59:43 +0000

    perf: try boosting perf on mobile devices

--- src/ts/critical.ts
+++ src/ts/critical.ts
@@ -86,50 +86,51 @@ try {
             });
         };
 
-        // Use the window load event to keep the page load performant
-        const wb = new Workbox("/sw.js");
+        window.addEventListener("load", () => {  
+            // Use the window load event to keep the page load performant
+            const wb = new Workbox("/sw.js");
 
-        // Add an event listener to detect when the registered
-        // service worker has installed but is waiting to activate.
-        wb.addEventListener("waiting", (event) => {
-            // `event.wasWaitingBeforeRegister` will be false if this is
-            // the first time the updated service worker is waiting.
-            // When `event.wasWaitingBeforeRegister` is true, a previously
-            // updated service worker is still waiting.
-            // You may want to customize the UI prompt accordingly.
+            // Add an event listener to detect when the registered
+            // service worker has installed but is waiting to activate.
+            wb.addEventListener("waiting", (event) => {
+                // `event.wasWaitingBeforeRegister` will be false if this is
+                // the first time the updated service worker is waiting.
+                // When `event.wasWaitingBeforeRegister` is true, a previously
+                // updated service worker is still waiting.
+                // You may want to customize the UI prompt accordingly.
 
-            // Assumes your app has some sort of prompt UI element
-            // that a user can either accept or reject.
-            dialog("confirm")
-                .then(() => {
+                // Assumes your app has some sort of prompt UI element
+                // that a user can either accept or reject.
+                dialog("confirm")
+                    .then(() => {
 
-                    wb.messageSkipWaiting();
-                })
-                .catch(() => { });
+                        wb.messageSkipWaiting();
+                    })
+                    .catch(() => { });
+            });
+
+
+            // Assuming the user accepted the update, set up a listener
+            // that will reload the page as soon as the previously waiting
+            // service worker has taken control.
+            wb.addEventListener("controlling", (event) => {
+                window.location.reload();
+            });
+
+            wb.addEventListener("activated", (event) => {
+                // `event.isUpdate` will be true if another version of the service
+                // worker was controlling the page when this version was registered.
+                if (!event.isUpdate) {
+                    console.log("Service worker activated for the first time!");
+
+                    // If your service worker is configured to precache assets, those
+                    // assets should all be available now.
+                    dialog("alert").catch(() => { });
+                }
+            });
+
+            wb.register();
         });
-
-        // Assuming the user accepted the update, set up a listener
-        // that will reload the page as soon as the previously waiting
-        // service worker has taken control.
-        wb.addEventListener("controlling", (event) => {
-            window.location.reload();
-        });
-
-        wb.addEventListener("activated", (event) => {
-            // `event.isUpdate` will be true if another version of the service
-            // worker was controlling the page when this version was registered.
-            if (!event.isUpdate) {
-                console.log("Service worker activated for the first time!");
-
-                // If your service worker is configured to precache assets, those
-                // assets should all be available now.
-                dialog("alert").catch(() => { });
-            }
-        });
-
-        // window.addEventListener("load", () => {  
-        // });
-        wb.register();
     }
 })();
 
@@ -166,6 +167,7 @@ const hasNetwork = (online: boolean) => {
     });
 };
 
+hasNetwork(navigator.onLine);
 window.addEventListener("load", () => {
     hasNetwork(navigator.onLine);
 
@@ -182,3 +184,4 @@ window.addEventListener("load", () => {
 
 // Accordion
 Accordion.run();
+export { };
