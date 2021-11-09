7b117def927e1b8480aca24d14875624416b9ee3
Okiki <okikio.dev@gmail.com>
2021-11-09 02:31:26 +0000

    fix: re-enable sw

--- src/ts/critical.ts
+++ src/ts/critical.ts
@@ -24,115 +24,115 @@ try {
 }
 
 // Check that service workers are supported
-// (async () => {
-//     if ("serviceWorker" in navigator) {
-//         let reloadDialog = document.querySelector(
-//             ".info-prompt.reload"
-//         ) as HTMLElement;
-//         let offlineDialog = document.querySelector(
-//             ".info-prompt.offline-ready"
-//         ) as HTMLElement;
+(async () => {
+    if ("serviceWorker" in navigator) {
+        let reloadDialog = document.querySelector(
+            ".info-prompt.reload"
+        ) as HTMLElement;
+        let offlineDialog = document.querySelector(
+            ".info-prompt.offline-ready"
+        ) as HTMLElement;
 
-//         const dialog = (type: "confirm" | "alert" = "confirm") => {
-//             let dialogEl = type == "confirm" ? reloadDialog : offlineDialog;
+        const dialog = (type: "confirm" | "alert" = "confirm") => {
+            let dialogEl = type == "confirm" ? reloadDialog : offlineDialog;
 
-//             return new Promise<void>((resolve, reject) => {
-//                 let dismissBtn = dialogEl?.querySelector(
-//                     ".dismiss"
-//                 ) as HTMLElement;
-//                 let acceptBtn = dialogEl?.querySelector(
-//                     ".accept"
-//                 ) as HTMLElement;
+            return new Promise<void>((resolve, reject) => {
+                let dismissBtn = dialogEl?.querySelector(
+                    ".dismiss"
+                ) as HTMLElement;
+                let acceptBtn = dialogEl?.querySelector(
+                    ".accept"
+                ) as HTMLElement;
 
-//                 let animateIn = {
-//                     target: dialogEl,
-//                     translateY: [200, 0],
-//                     visibility: ["visible", "visible"],
-//                     fillMode: "both",
-//                     easing: "ease",
-//                     duration: 350
-//                 };
+                let animateIn = {
+                    target: dialogEl,
+                    translateY: [200, 0],
+                    visibility: ["visible", "visible"],
+                    fillMode: "both",
+                    easing: "ease",
+                    duration: 350
+                };
 
-//                 let animateOut = {
-//                     ...animateIn,
-//                     translateY: [0, 200],
-//                     visibility: ["visible", "hidden"],
-//                     delay: 3000,
-//                 };
+                let animateOut = {
+                    ...animateIn,
+                    translateY: [0, 200],
+                    visibility: ["visible", "hidden"],
+                    delay: 3000,
+                };
 
-//                 animate(animateIn).then(() => type != "confirm" ? animate(animateOut) : null);
+                animate(animateIn).then(() => type != "confirm" ? animate(animateOut) : null);
 
-//                 if (dismissBtn) {
-//                     dismissBtn.onclick = () => {
-//                         animate({
-//                             ...animateOut,
-//                             delay: 0,
-//                         });
+                if (dismissBtn) {
+                    dismissBtn.onclick = () => {
+                        animate({
+                            ...animateOut,
+                            delay: 0,
+                        });
 
-//                         reject();
-//                     };
-//                 }
+                        reject();
+                    };
+                }
 
-//                 if (acceptBtn) {
-//                     acceptBtn.onclick = () => {
-//                         animate({
-//                             ...animateOut,
-//                             delay: 0,
-//                         });
+                if (acceptBtn) {
+                    acceptBtn.onclick = () => {
+                        animate({
+                            ...animateOut,
+                            delay: 0,
+                        });
 
-//                         resolve();
-//                     };
-//                 }
-//             });
-//         };
+                        resolve();
+                    };
+                }
+            });
+        };
 
-//         window.addEventListener("load", () => {  
-//             // Use the window load event to keep the page load performant
-//             const wb = new Workbox("/sw.js");
+        window.addEventListener("load", () => {  
+            // Use the window load event to keep the page load performant
+            const wb = new Workbox("/sw.js");
 
-//             // Add an event listener to detect when the registered
-//             // service worker has installed but is waiting to activate.
-//             wb.addEventListener("waiting", (event) => {
-//                 // `event.wasWaitingBeforeRegister` will be false if this is
-//                 // the first time the updated service worker is waiting.
-//                 // When `event.wasWaitingBeforeRegister` is true, a previously
-//                 // updated service worker is still waiting.
-//                 // You may want to customize the UI prompt accordingly.
+            // Add an event listener to detect when the registered
+            // service worker has installed but is waiting to activate.
+            wb.addEventListener("waiting", (event) => {
+                // `event.wasWaitingBeforeRegister` will be false if this is
+                // the first time the updated service worker is waiting.
+                // When `event.wasWaitingBeforeRegister` is true, a previously
+                // updated service worker is still waiting.
+                // You may want to customize the UI prompt accordingly.
 
-//                 // Assumes your app has some sort of prompt UI element
-//                 // that a user can either accept or reject.
-//                 dialog("confirm")
-//                     .then(() => {
+                // Assumes your app has some sort of prompt UI element
+                // that a user can either accept or reject.
+                dialog("confirm")
+                    .then(() => {
 
-//                         wb.messageSkipWaiting();
-//                     })
-//                     .catch(() => { });
-//             });
+                        wb.messageSkipWaiting();
+                    })
+                    .catch(() => { });
+            });
 
 
-//             // Assuming the user accepted the update, set up a listener
-//             // that will reload the page as soon as the previously waiting
-//             // service worker has taken control.
-//             wb.addEventListener("controlling", (event) => {
-//                 window.location.reload();
-//             });
+            // Assuming the user accepted the update, set up a listener
+            // that will reload the page as soon as the previously waiting
+            // service worker has taken control.
+            wb.addEventListener("controlling", (event) => {
+                window.location.reload();
+            });
 
-//             wb.addEventListener("activated", (event) => {
-//                 // `event.isUpdate` will be true if another version of the service
-//                 // worker was controlling the page when this version was registered.
-//                 if (!event.isUpdate) {
-//                     console.log("Service worker activated for the first time!");
+            wb.addEventListener("activated", (event) => {
+                // `event.isUpdate` will be true if another version of the service
+                // worker was controlling the page when this version was registered.
+                if (!event.isUpdate) {
+                    console.log("Service worker activated for the first time!");
 
-//                     // If your service worker is configured to precache assets, those
-//                     // assets should all be available now.
-//                     dialog("alert").catch(() => { });
-//                 }
-//             });
+                    // If your service worker is configured to precache assets, those
+                    // assets should all be available now.
+                    dialog("alert").catch(() => { });
+                }
+            });
 
-//             wb.register();
-//         });
-//     }
-// })();
+            wb.register();
+        });
+    }
+})();
 
 // navbar focus on scroll effect
 let canScroll = true;
