c747ed963874028de7a312e2051d9bfc4bfa156a
Okiki Ojo <okikio.dev@gmail.com>
2022-02-24 08:30:42 +0000

    feat: pkg versioning using proper package.json
    bundle.js.org now actively loads up the package.json for individual packages to ensure proper dependency versions. Switched to using unpkg.com as default CDN, because it's easier to traverse

--- src/ts/plugins/external.ts
+++ src/ts/plugins/external.ts
@@ -44,12 +44,24 @@ export const PolyfillMap = {
     process: 'process/browser',
     fs: 'memfs',
     os: 'os-browserify/browser',
+    'v8': "v8",
+    "node-inspect": "node-inspect",
+    "_linklist": "_linklist",
+    "_stream_wrap": "_stream_wrap"
 };
 
 export const PolyfillKeys = Object.keys(PolyfillMap);
-
 export const DeprecatedAPIs = ["v8/tools/codemap", "v8/tools/consarray", "v8/tools/csvparser", "v8/tools/logreader", "v8/tools/profile_view", "v8/tools/profile", "v8/tools/SourceMap", "v8/tools/splaytree", "v8/tools/tickprocessor-driver", "v8/tools/tickprocessor", "node-inspect/lib/_inspect", "node-inspect/lib/internal/inspect_client ", "node-inspect/lib/internal/inspect_repl", "_linklist", "_stream_wrap"];
 export const ExternalPackages = ['chokidar', 'yargs', 'fsevents', `worker_threads`, "assert/strict", "async_hooks", "diagnostics_channel", "http2", "fs/promises", "inspector", "perf_hooks", "timers/promises", "trace_events", "v8", "wasi", ...DeprecatedAPIs, ...PolyfillKeys];
+
+// Based on https://github.com/egoist/play-esbuild/blob/7e34470f9e6ddcd9376704cd8b988577ddcd46c9/src/lib/esbuild.ts#L51
+export const isExternal = (id: string) => {
+    return ExternalPackages.find((it: string): boolean => {
+        if (it === id) return true; // import 'foo' & external: ['foo']
+        if (id.startsWith(`${it}/`)) return true; // import 'foo/bar.js' & external: ['foo']
+        return false;
+    });
+}
 
 export const EXTERNALS_NAMESPACE = 'external-globals';
 export const EXTERNAL = (): Plugin => {
@@ -66,7 +78,7 @@ export const EXTERNAL = (): Plugin => {
 
             build.onResolve({ filter: /.*/ }, (args) => {
                 let path = args.path.replace(/^node\:/, "");
-                if (ExternalPackages.includes(path)) {
+                if (isExternal(path)) {
                     return {
                         path,
                         namespace: EXTERNALS_NAMESPACE,
