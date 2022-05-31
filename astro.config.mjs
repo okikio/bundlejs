b0d864f954f8dbaf2782f421fce21ff41995c360
Okiki Ojo <okikio.dev@gmail.com>
2022-05-31 22:38:23 +0000

    chore: ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -57,46 +57,46 @@ export default defineConfig({
     sitemap(),
     // compress(),
     // serviceWorker({
-  //   workbox: {
-  //       globDirectory: destFolder,
-  //       globPatterns: [
-  //           "**/*.{html,js,css}",
-  //           "/js/*.ttf",
-  //           "/favicon/*.svg",
-  //           "!/js/index.min.css",
-  //       ],
-  //       ignoreURLParametersMatching: [/index\.html\?(.*)/, /\\?(.*)/],
-  //       cleanupOutdatedCaches: true,
-  //       // Define runtime caching rules.
-  //       runtimeCaching: [
-  //           {
-  //               // Match any request that starts with https://api.producthunt.com, https://api.countapi.xyz, https://opencollective.com, etc...
-  //               urlPattern:
-  //                   /^https:\/\/((?:api\.producthunt\.com)|(?:api\.countapi\.xyz)|(?:opencollective\.com)|(?:discus\.bundlejs\.com)|(?:analytics\.bundlejs\.com))/,
-  //               // Apply a network-first strategy.
-  //               handler: "NetworkFirst",
-  //               method: "GET",
-  //               options: {
-  //                 cacheableResponse: {
-  //                   statuses: [0, 200]
-  //                 },
-  //               }
-  //           },
-  //           {
-  //               // Match any request that ends with .png, .jpg, .jpeg, .svg, etc....
-  //               urlPattern:
-  //                   /workbox\-(.*).js|\.(?:png|jpg|jpeg|svg|webp|map|wasm|json|ts|css)$|^https:\/\/(?:cdn\.polyfill\.io)/,
-  //               // Apply a stale-while-revalidate strategy.
-  //               handler: "StaleWhileRevalidate",
-  //               method: "GET",
-  //               options: {
-  //                   cacheableResponse: {
-  //                       statuses: [0, 200]
-  //                   }
-  //               }
-  //           },
-  //         ]
-  //   }
+    //   workbox: {
+    //       globDirectory: destFolder,
+    //       globPatterns: [
+    //           "**/*.{html,js,css}",
+    //           "/js/*.ttf",
+    //           "/favicon/*.svg",
+    //           "!/js/index.min.css",
+    //       ],
+    //       ignoreURLParametersMatching: [/index\.html\?(.*)/, /\\?(.*)/],
+    //       cleanupOutdatedCaches: true,
+    //       // Define runtime caching rules.
+    //       runtimeCaching: [
+    //           {
+    //               // Match any request that starts with https://api.producthunt.com, https://api.countapi.xyz, https://opencollective.com, etc...
+    //               urlPattern:
+    //                   /^https:\/\/((?:api\.producthunt\.com)|(?:api\.countapi\.xyz)|(?:opencollective\.com)|(?:discus\.bundlejs\.com)|(?:analytics\.bundlejs\.com))/,
+    //               // Apply a network-first strategy.
+    //               handler: "NetworkFirst",
+    //               method: "GET",
+    //               options: {
+    //                 cacheableResponse: {
+    //                   statuses: [0, 200]
+    //                 },
+    //               }
+    //           },
+    //           {
+    //               // Match any request that ends with .png, .jpg, .jpeg, .svg, etc....
+    //               urlPattern:
+    //                   /workbox\-(.*).js|\.(?:png|jpg|jpeg|svg|webp|map|wasm|json|ts|css)$|^https:\/\/(?:cdn\.polyfill\.io)/,
+    //               // Apply a stale-while-revalidate strategy.
+    //               handler: "StaleWhileRevalidate",
+    //               method: "GET",
+    //               options: {
+    //                   cacheableResponse: {
+    //                       statuses: [0, 200]
+    //                   }
+    //               }
+    //           },
+    //         ]
+    //   }
     // })
   ],
   vite: {
