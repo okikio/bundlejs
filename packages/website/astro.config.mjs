78953236a442af35d67f60bd137b0400c4cab44f
Okiki Ojo <okikio.dev@gmail.com>
2022-06-14 04:13:14 +0000

    chore: ...

--- packages/website/astro.config.mjs
+++ packages/website/astro.config.mjs
@@ -1,65 +1,56 @@
 import { defineConfig } from "astro/config";
-import serviceWorker from 'astro-service-worker';
-
-import * as path from "node:path";
-import { lookupCollection } from '@iconify/json';
 
 import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
 import AutoImport from 'unplugin-auto-import/vite';
 
-import solid from "@astrojs/solid-js";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import { lookupCollection } from '@iconify/json';
+
+import SolidJS from "@astrojs/solid-js";
+import Tailwind from "@astrojs/tailwind";
+import Sitemap from "@astrojs/sitemap";
+
+import ServiceWorker from 'astro-service-worker';
+import Compress from "astro-compress";
+
+import { s as svg } from "hastscript";
+import { PRODUCTION_MODE } from "./env.mjs";
 
 import { outDir } from "./shared.config.cjs";
-
-import compress from "astro-compress";
-import { h, s } from "hastscript";
-
 const { icons: fluentIcons } = await lookupCollection("fluent");
-const IconLink = fluentIcons["link-24-regular"];
-const IconArrow = fluentIcons["arrow-up-right-24-regular"];
 
-const __dirname = path.resolve(path.dirname(""));
+/**
+ * @param {string} icon 
+ * @param {typeof fluentIcons} iconlist
+ * @returns SVG Element
+ */
+function createSVG (icon, iconlist = fluentIcons) { 
+  let IconData = iconlist[icon];
+  return svg("svg", {
+    width: IconData.width,
+    height: IconData.height,
+    viewBox: `0 0 ${IconData.width} ${IconData.height}`,
+    class: "icon",
+    "rehype-icon": icon,
+  }, [IconData.body])
+}
 
 // https://astro.build/config
 export default defineConfig({
   outDir,
-  build: {
-    // Example: Generate `page.html` instead of `page/index.html` during build.
-    format: "file"
-  },
+  build: { format: "file" },
   site: "https://bundlejs.com",
   markdown: {
     rehypePlugins: [
       "rehype-slug",
       ["rehype-autolink-headings", {
-        behavior: "append",
-        content: [
-          s("svg", {
-            xmlns: "http://www.w3.org/2000/svg",
-            width: IconLink.width,
-            height: IconLink.height,
-            viewBox: `0 0 ${IconLink.width} ${IconLink.height}`,
-            class: "icon"
-          }, [IconLink.body])
-        ],
+        behavior: "prepend",
+        content: [ createSVG("link-24-regular") ],
         test: ['h2', 'h3', 'h4', 'h5', 'h6', 'details', 'summary', 'astro-root']
       }],
       ["rehype-external-links", {
-        target: "_blank",
-        rel: ["noopener"],
-        content: [
-          s("svg", {
-            xmlns: "http://www.w3.org/2000/svg",
-            width: IconArrow.width,
-            height: IconArrow.height,
-            viewBox: `0 0 ${IconArrow.width} ${IconArrow.height}`,
-            class: "icon",
-            "rehype-icon": "arrow-up-right-24-regular",
-          }, [IconArrow.body])
-        ]
+        target: "_blank", rel: ["noopener"],
+        content: [ createSVG("arrow-up-right-24-regular") ]
       }]
     ],
     shikiConfig: {
@@ -69,64 +60,15 @@ export default defineConfig({
     }
   },
   integrations: [
-    solid(),
-    tailwind({
-      config: {
-        applyBaseStyles: false
-      }
-    }),
-    sitemap(),
-    compress(),
-    // serviceWorker({
-    //   workbox: {
-    //       globDirectory: destFolder,
-    //       globPatterns: [
-    //           "**/*.{html,js,css}",
-    //           "/js/*.ttf",
-    //           "/favicon/*.svg",
-    //           "!/js/index.min.css",
-    //       ],
-    //       ignoreURLParametersMatching: [/index\.html\?(.*)/, /\\?(.*)/],
-    //       cleanupOutdatedCaches: true,
-    //       // Define runtime caching rules.
-    //       runtimeCaching: [
-    //           {
-    //               // Match any request that starts with https://api.producthunt.com, https://api.countapi.xyz, https://opencollective.com, etc...
-    //               urlPattern:
-    //                   /^https:\/\/((?:api\.producthunt\.com)|(?:api\.countapi\.xyz)|(?:opencollective\.com)|(?:discus\.bundlejs\.com)|(?:analytics\.bundlejs\.com))/,
-    //               // Apply a network-first strategy.
-    //               handler: "NetworkFirst",
-    //               method: "GET",
-    //               options: {
-    //                 cacheableResponse: {
-    //                   statuses: [0, 200]
-    //                 },
-    //               }
-    //           },
-    //           {
-    //               // Match any request that ends with .png, .jpg, .jpeg, .svg, etc....
-    //               urlPattern:
-    //                   /workbox\-(.*).js|\.(?:png|jpg|jpeg|svg|webp|map|wasm|json|ts|css)$|^https:\/\/(?:cdn\.polyfill\.io)/,
-    //               // Apply a stale-while-revalidate strategy.
-    //               handler: "StaleWhileRevalidate",
-    //               method: "GET",
-    //               options: {
-    //                   cacheableResponse: {
-    //                       statuses: [0, 200]
-    //                   }
-    //               }
-    //           },
-    //         ]
-    //   }
-    // })
+    SolidJS(),
+    Tailwind({ config: { applyBaseStyles: false } }),
+    Sitemap(),
+    PRODUCTION_MODE ? Compress() : { name: "blank", hooks: {} },
   ],
+  experimental: { integrations: true },
   vite: {
-    build: {
-      assetsInlineLimit: 0
-    },
-    ssr: {
-      external: ["svgo"]
-    },
+    build: { assetsInlineLimit: 0 },
+    ssr: { external: ["svgo"] },
     plugins: [
       AutoImport({
         resolvers: [
