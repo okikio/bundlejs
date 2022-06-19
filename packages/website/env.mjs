9492df6bf4eb568301d84d3c7811bca6171b10d4
Okiki Ojo <okikio.dev@gmail.com>
2022-06-19 00:51:27 +0000

    chore: ...

--- packages/website/env.mjs
+++ packages/website/env.mjs
@@ -1,8 +1,9 @@
-export const ENABLE_ALL = Boolean(import.meta?.env?.ENABLE_ALL) ?? true;
-export const ENABLE_SW = ENABLE_ALL ?? Boolean(import.meta?.env?.ENABLE_SW) ?? true;
-export const USE_SHAREDWORKER = ENABLE_ALL ?? Boolean(import.meta?.env?.USE_SHAREDWORKER) ?? true;
-export const PRODUCTION_MODE = ENABLE_ALL ?? Boolean(import.meta?.env?.PRODUCTION_MODE) ?? true;
+const env = import.meta?.env ?? globalThis?.process?.env;
 
+export const ENABLE_ALL = "ENABLE_ALL" in env ? Boolean(env?.ENABLE_ALL) : false;
+export const ENABLE_SW = "ENABLE_SW" in env ? Boolean(env?.ENABLE_SW) : (ENABLE_ALL ?? true);
+export const USE_SHAREDWORKER = "USE_SHAREDWORKER" in env ? Boolean(env?.USE_SHAREDWORKER) : (ENABLE_ALL ?? true);
+export const PRODUCTION_MODE = "PRODUCTION_MODE" in env ? Boolean(env?.PRODUCTION_MODE) : (ENABLE_ALL ?? true);
 
 // ServiceWorker({
 //   workbox: {
