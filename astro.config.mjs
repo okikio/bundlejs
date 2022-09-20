6eec126fa0dbdd54b90bfeaa6e40c6b878bed895
Okiki <okikio.dev@gmail.com>
2022-09-20 05:51:14 +0000

    ...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  build: { format: "file" },
+  build: {
+    format: "file"
+  },
   site: "https://bundlejs.com",
   integrations: [
     tailwind({
-      config: { applyBaseStyles: false }
-    })
+      config: {
+        applyBaseStyles: false
+      }
+    }), 
+    sitemap()
   ]
 });
