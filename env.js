12a004b12f5483baec64769980a067ac2adfd710
Okiki <okikio.dev@gmail.com>
2023-02-16 17:07:54 +0000

    disable sharedworkers

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-export const ENABLE_ALL = true;
-export const ENABLE_SW = ENABLE_ALL ?? true;
-export const USE_SHAREDWORKER = ENABLE_ALL ?? true;
-export const PRODUCTION_MODE = ENABLE_ALL ?? true;
+export const ENABLE_ALL = false;
+export const ENABLE_SW = true; // ENABLE_ALL ?? 
+export const USE_SHAREDWORKER = false; // ENABLE_ALL ?? true
+export const PRODUCTION_MODE = true; // ENABLE_ALL ?? 
