3be8ce86337ced4eafefde168ffc6b37cba4008c
Okiki <okikio.dev@gmail.com>
2023-12-19 22:00:39 +0000

    feat: support loading different versions of esbuild
    Signed-off-by: Okiki <okikio.dev@gmail.com>

--- packages/core/src/utils/get-esbuild.ts
+++ packages/core/src/utils/get-esbuild.ts
@@ -1,23 +1,23 @@
 import type * as ESBUILD from "esbuild-wasm";
-
 import type { Platform } from "../configs/platform.ts";
-import { PLATFORM_AUTO } from "../configs/platform.ts";
-import pkg from "../../node_modules/esbuild-wasm/package.json" assert { type: "json" };
 
-const { version } = pkg;
+import { PLATFORM_AUTO } from "../configs/platform.ts";
+import pkg from "../../node_modules/esbuild-wasm/package.json" with { type: "json" };
+const { version: defaultVersion } = pkg;
 
 /**
  * Determines which esbuild skew to use depending on the platform option supplied, 
- * by default it will choose the most perfomnant esbuild skew, 
+ * by default it will choose the most performant esbuild skew, 
  * so on deno and node it will choose the native while on browsers it will choose WASM.
  * 
  * You can specifiy which platform skew you would like, 
  * for example you can choose "deno-wasm" as a skew, where you can run the esbuild but in WASM
  * 
  * @param platform Which platform skew of esbuild should be used
+ * @param version Which esbuild version to load
  * @returns esbuild module
  */
-export async function getEsbuild(platform: Platform = PLATFORM_AUTO): Promise<typeof ESBUILD> {
+export async function getEsbuild(platform: Platform = PLATFORM_AUTO, version = defaultVersion): Promise<typeof ESBUILD> {
   try {
     switch (platform) {
       case "deno":
