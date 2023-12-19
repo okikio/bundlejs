d10293bff674a4f7993ffc66939118155e577fbf
Okiki <okikio.dev@gmail.com>
2023-12-19 12:07:20 +0000

    ci: update core's build-wasm script
    Signed-off-by: Okiki <okikio.dev@gmail.com>

--- packages/core/scripts/build-wasm.ts
+++ packages/core/scripts/build-wasm.ts
@@ -1,48 +1,48 @@
-import { encode } from "@bundle/compress/deno/base64/mod.ts";
-import { compress, decompress } from "@bundle/compress/deno/lz4/mod.ts";
+import { base64, ascii85 } from "@bundle/utils/utils/encoding.ts";
+import { outdent } from "@bundle/utils/utils/outdent.ts";
+
+import { compress as lz4, decompress as unlz4 } from "@bundle/compress/deno/lz4/mod.ts";
 import { compress as gzip, decompress as gunzip } from "@bundle/compress/deno/gzip/mod.ts";
 
 import { bytes } from "@bundle/utils/utils/pretty-bytes.ts";
 
 import { compress as brotli, decompress as unbrotli } from "@bundle/compress/deno/brotli/mod.ts";
-import { init, compress as zstdCompress, decompress as zstdDecompress } from "@bundle/compress/deno/zstd/mod.ts";
+import { compress as zstd, decompress as unzstd } from "@bundle/compress/deno/zstd/mod.ts";
 
 import { dirname, relative } from 'node:path';
 import * as fs from "node:fs/promises";
 const encoder = new TextEncoder();
 
-await init();
-
-export async function build(mode: "zstd" | "brotli" | "gzip" | "lz4" | "base64" = "zstd", src: string | Uint8Array | Promise<string | Uint8Array> = "./node_modules/esbuild-wasm/esbuild.wasm", target = "src/wasm.ts") {
+export async function build([mode = "zstd", encoding = "base64"]: Partial<["zstd" | "brotli" | "gzip" | "lz4" | "none", "base64" | "ascii85"]> = [], src: string | Uint8Array | Promise<string | Uint8Array> = "./node_modules/esbuild-wasm/esbuild.wasm", target = "src/wasm.ts", importsPaths?: Partial<Record<"gzip" | "zstd" | "lz4" | "brotli", string>>) {
   const value = await src;
 
-  if (typeof value == "string") console.log(`\n- Source file: ${value}`);
-  const res = typeof value == "string" ? await fs.readFile(value) : value;
+  if (typeof value === "string") console.log(`\n- Source file: ${value}`);
+  const res = typeof value === "string" ? await fs.readFile(value) : value;
   const wasm = new Uint8Array(res);
   console.log(`- Read WASM (size: ${bytes(wasm.length)} bytes)`);
 
   console.time("Compression time")
   let compressed: Uint8Array = wasm;
-  if (mode == "zstd") {
-    compressed = await zstdCompress(wasm, 22);
-  } else if (mode == "brotli") {
+  if (mode === "zstd") {
+    compressed = await zstd(wasm, 22);
+  } else if (mode === "brotli") {
     compressed = await brotli(wasm);
-  } else if (mode == "gzip") {
+  } else if (mode === "gzip") {
     compressed = await gzip(wasm);
-  } else if (mode == "lz4") {
-    compressed = await compress(wasm);
+  } else if (mode === "lz4") {
+    compressed = await lz4(wasm);
   }
   console.timeEnd("Compression time")
 
   console.time("Decompression time")
-  if (mode == "zstd") {
-    await zstdDecompress(compressed);
-  } else if (mode == "brotli") {
+  if (mode === "zstd") {
+    await unzstd(compressed);
+  } else if (mode === "brotli") {
     await unbrotli(compressed);
-  } else if (mode == "gzip") {
+  } else if (mode === "gzip") {
     await gunzip(compressed);
-  } else if (mode == "lz4") {
-    await decompress(wasm);
+  } else if (mode === "lz4") {
+    await unlz4(wasm);
   }
   console.timeEnd("Decompression time")
 
@@ -50,9 +50,9 @@ export async function build(mode: "zstd" | "brotli" | "gzip" | "lz4" | "base64" 
     `- Compressed WASM using ${mode} (reduction: ${bytes(wasm.length - compressed.length)} bytes, size: ${bytes(compressed.length)})`,
   );
 
-  const encoded = encode(compressed);
+  const encoded = JSON.stringify(encoding === "ascii85" ? ascii85.encodeAscii85(compressed) : base64.encodeBase64(compressed));
   console.log(
-    `- Encoded WASM using base64, (increase: ${bytes(encoded.length -
+    `- Encoded WASM using ${encoding}, (increase: ${bytes(encoded.length -
       compressed.length)}, size: ${bytes(encoded.length)})`,
   );
 
@@ -60,8 +60,6 @@ export async function build(mode: "zstd" | "brotli" | "gzip" | "lz4" | "base64" 
   console.log({
     target,
     targetDir,
-    to: "@bundle/compress/deno/zstd/mod.ts",
-    path: relative(targetDir, "@bundle/compress/deno/zstd/mod.ts")
   })
 
   console.log("- Inlining wasm code in js");
@@ -69,17 +67,37 @@ export async function build(mode: "zstd" | "brotli" | "gzip" | "lz4" | "base64" 
     "gzip": "@bundle/compress/deno/gzip/mod.ts",
     "brotli": "@bundle/compress/deno/brotli/mod.ts",
     "zstd": "@bundle/compress/deno/zstd/mod.ts",
-    "lz4": "@bundle/compress/deno/lz4/mod.ts"
+    "lz4": "@bundle/compress/deno/lz4/mod.ts",
+    ...importsPaths
   })[mode];
-  const source = `\
-  // @ts-nocheck
-  export const source = async () => {
-    const uint8arr = Uint8Array.from(atob("${encoded}"), c => c.charCodeAt(0));
-    ${mode == "base64" ? `return uint8arr;` : `
+
+  const commonReturn = outdent`
     const { decompress } = await import(\"${compressionModuleImportPath}\");
-    return await decompress(uint8arr);`}
-  };
-  export default source;`;
+    return await decompress(uint8arr);
+  `;
+
+  const modeReturns = ({
+    "gzip": outdent`
+        const cs = new DecompressionStream('gzip');
+        const decompressedStream = new Blob([uint8arr]).stream().pipeThrough(cs);
+        return new Uint8Array(await new Response(decompressedStream).arrayBuffer());
+    `,
+    "brotli": commonReturn,
+    "lz4": commonReturn,
+    "zstd": commonReturn,
+  })[mode] ?? `return uint8arr;`;
+
+  const source = outdent`
+    ${encoding === "ascii85" ? `import { ascii85 } from "@bundle/utils/utils/encoding.ts";` : ""}
+    export const source = async () => {
+      const uint8arr = (${encoding === "ascii85" ?
+      `ascii85.decodeAscii85(\n\t${encoded}\n)` :
+      `Uint8Array.from(atob(${encoded}), c => c.charCodeAt(0))`
+    });
+      ${modeReturns}
+    };
+    export default source;
+  `;
 
   console.log(`- Writing output to file (${target})`);
   await Promise.all([
@@ -93,10 +111,4 @@ export async function build(mode: "zstd" | "brotli" | "gzip" | "lz4" | "base64" 
   );
 }
 
-await build("zstd");
-
-// import { source as brotliWASM } from "@bundle/compress/deno/brotli/wasm";
-// import { source as lz4WASM } from "@bundle/compress/deno/lz4/wasm";
-
-// await build("zstd", new Uint8Array(await lz4WASM()), "src/deno/lz4/wasm-2.ts");
-// await build("zstd", await brotliWASM(), "src/deno/brotli/wasm-3.ts");
+await build(["zstd", "ascii85"]);
