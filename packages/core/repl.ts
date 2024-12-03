6f1ce6e0ecc0ee71ba0ec3571fa11018d6e7d20d
Okiki <hey@okikio.dev>
2024-12-03 16:31:51 +0000

    chore: ...
    Signed-off-by: Okiki <hey@okikio.dev>

--- packages/core/repl.ts
+++ packages/core/repl.ts
@@ -1,43 +1,69 @@
-import { build, compress, getFile, setFile, PLATFORM_AUTO, TheFileSystem } from "./src/index.ts";
+import type { TarStreamEntry } from "@std/tar/untar-stream";
+import { UntarStream } from "@std/tar/untar-stream";
+import { normalize, join } from "@std/path/posix";
 
-const fs = await TheFileSystem;
+const response = await fetch("https://pkg.pr.new/@tanstack/react-query@7988");
+const content = await response.arrayBuffer();
 
-console.log("\n");
-await setFile(fs, "/index.tsx", `\
-export * as Other from "/new.tsx";
-export * from "spring-easing";`);
-await setFile(fs, "/new.tsx", "export * from \"@okikio/native\";");
-await setFile(fs, "/other.tsx", `\
-export * as Other from "/index.tsx";
-export * from "@okikio/emitter";`);
-
-console.log(await getFile(fs, "/index.tsx", "string") )
-console.log(fs)
-
-const result = await build({
-  entryPoints: ["/index.tsx", "/new.tsx"],
-  esbuild: {
-    treeShaking: true,
-    splitting: true,
-    format: "esm"
-  },
-});
-
-console.log(
-  await compress(
-    result.contents.map((x: any) => x?.contents),
-    { type: "gzip" }
-  )
-);
+const contentType = response.headers.get("content-type");
 
 
-if (PLATFORM_AUTO === "deno") {
-  globalThis?.Deno?.exit?.();
-} else {
-  // @ts-ignore Only for Node
-  globalThis?.process?.exit?.();
+if (contentType?.trim()?.toLowerCase() === "application/tar+gzip") {
+  // const blob = new Blob([content]);
+  const stream = new Blob([content]).stream()
+    .pipeThrough<Uint8Array>(new DecompressionStream("gzip"))
+    .pipeThrough(new UntarStream());
+
+  // Create a reader from the stream
+  const reader = stream.getReader();
+
+  console.log({
+    stream,
+    reader
+  })
+
+  // Get the stream as an async iterable
+  const iterableStream = {
+    async *[Symbol.asyncIterator]() {
+      try {
+
+        console.log({
+          result: "result"
+        })
+
+        while (true) {
+          const result = await reader.read();
+          if (result.done) break;
+          yield result.value;
+        }
+
+        console.log({
+          result_POLL: "result"
+        })
+      } finally {
+        reader.releaseLock();
+      }
+    }
+  };
+
+  for await (const entry of iterableStream) {
+      // console.log({
+      //     entry
+      // })
+      const path = normalize(entry.path);
+      // await Deno.mkdir(dirname(path));
+
+      // Convert the stream into a Blob
+      const blob = await new Response(entry.readable).blob();
+
+      // Convert the Blob to an ArrayBuffer and then into a Uint8Array
+      const arrayBuffer = await blob.arrayBuffer();
+
+      const uint8arr = new Uint8Array(arrayBuffer);
+      console.log({
+        path: join("./", path)
+      })
+      // setFile(args.namespace + ":" + join("./", path), uint8arr);
+  }
+
 }
-
-
-// import { resolveVersion } from "./src/utils/npm-search";
-// console.log(await resolveVersion("@okikio/animate@>=1 <2"))
